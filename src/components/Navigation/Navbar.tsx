163a420ee69ce981230249507a34bffd4221bbef
PierreVelluet <velluetp@gmail.com>
2023-07-03 12:29:20 +0900

    first commit

---
+++ src/components/Navigation/Navbar.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import * as React from "react";
+
+import { useRouter } from 'next/navigation'
+import Link from "next/link";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import FiberNewIcon from "@mui/icons-material/FiberNew";
+import { usePathname } from 'next/navigation'
+
+const pages = [
+    { name: "Jeux vidéo", ref: "games" },
+    { name: "Matériel grimpe", ref: "" },
+];
+const settings = ["Nique", "ta", "mère", "stp"];
+
+function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const router = useRouter();
+    const isPageActive = (ref:string):boolean => {
+        return usePathname() == "/"+ref;
+    } 
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleClickitemNavigation = (path:string) => {
+        router.push(path ?? "/");
+    };
+
+    const handleCloseUserMenu = () => {
+    };
+
+    return (
+        <AppBar position="fixed" sx={{ mb: 5 }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <FiberNewIcon sx={{ fontSize: "70px", display: { m: "none", md: "flex" }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: "none", md: "flex" },
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                        }}></Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit">
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleClickitemNavigation}
+                            sx={{
+                                display: { xs: "block", md: "none" },
+                            }}>
+                            {pages.map((page) => (
+                                <Link key={page.ref} href={page.ref}>
+                                    <MenuItem onClick={() => handleClickitemNavigation(page.ref)}>
+                                        <Typography textAlign="center">{page.ref}</Typography>
+                                    </MenuItem>
+                                </Link>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: "flex", md: "none" },
+                            flexGrow: 1,
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                        }}>
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                        {pages.map((page) => (
+                                <Button key={page.ref} onClick={() => handleClickitemNavigation(page.ref)} sx={{ my: 2, color: isPageActive(page.ref) ? "red" : "white", display: "block" }}>
+                                    {page.name}
+                                </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: "45px" }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}>
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default ResponsiveAppBar;
