ee6efce27839143f3c82f09295b97fb0967d7882
PierreVelluet <velluetp@gmail.com>
2023-07-26 18:26:45 +0900

    failed globale state implementaiton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import "animate.css";
 import { Inter } from "next/font/google";
 import Navbar from "../components/Navigation/Navbar";
-import Providers from "./Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,12 +14,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Providers>
-                    <div className="mb-7">
-                        <Navbar />
-                    </div>
-                    {children}
-                </Providers>
+                <div className="mb-7">
+                    <Navbar />
+                </div>
+                {children}
             </body>
         </html>
     );
